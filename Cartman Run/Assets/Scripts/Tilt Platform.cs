bb31ed83bbb4d53558fdb108970896a13e0b87c5
Hyozzun <martin.mozier@gmail.com>
2024-09-14 13:28:24 -0700

    Added Tilt Controls
    Moved Camera to view a room, 40 * 40. Tilt controls using left, right, up, and down or wasd. r to reset room orientation. renamed inputs "horizontal" and "vertical" to "HorizontalKey" and "VerticalKey"

---
+++ Cartman Run/Assets/Scripts/Tilt Platform.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TiltPlatform : MonoBehaviour
+{
+    public Vector3 rotation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private int timer = 0;
+    private bool canRotate = true;
+    void Rotate()
+    {
+        if (Input.GetAxis("HorizontalKey") > 0.2)
+        {
+            if (rotation.z >= 344 || rotation.z <= 15)
+            {
+                transform.Rotate(0f, 0f, 0.5f);
+            }
+        }
+        if (Input.GetAxis("HorizontalKey") < -0.2)
+        {
+            if (rotation.z <= 16 || rotation.z >= 345)
+            {
+                transform.Rotate(0f, 0f, -0.5f);
+            }
+        }
+        if (Input.GetAxis("VerticalKey") > 0.2)
+        {
+            if (rotation.x >= 345 || rotation.x <= 16) 
+            {
+                transform.Rotate(-0.5f, 0f, 0f);
+            }
+        }
+        if (Input.GetAxis("VerticalKey") < -0.2)
+        {
+            if (rotation.x >= 344 || rotation.x <= 15)
+            {
+                transform.Rotate(0.5f, 0f, 0f);
+            }
+        }
+    }
+    void Update()
+    {
+        rotation = GetComponent<Transform>().eulerAngles;
+        timer++;
+        if (canRotate) {
+            Rotate();
+            canRotate = false;
+        }
+        if(timer >= 5)
+        {
+            canRotate = true;
+            timer = 0;
+        }
+        transform.Rotate(0, -rotation.y, 0);
+        if (Input.GetKeyDown("r"))
+        {
+            transform.Rotate(-rotation.x, 0, -rotation.z);
+            timer = 0;
+        }
+    }
+}
