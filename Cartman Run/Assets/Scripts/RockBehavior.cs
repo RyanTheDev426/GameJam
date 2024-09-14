8cfe3c3aff3848205c91fd68caf0547b024c22d5
RyanTheDev426 <orth.ryane@gmail.com>
2024-09-14 12:15:46 -0700

    Rock Projectile script

---
+++ Cartman Run/Assets/Scripts/RockBehavior.cs
@@ -0,0 +1,29 @@
+using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockBehavior : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 rockVelocity = new Vector3(10, 0, 0);
+
+        GameObject playerObject = GameObject.FindGameObjectWithTag("Player");
+        transform.LookAt(playerObject.transform.position);
+
+    }
+
+    void OnColliderEnter(Collider other)
+    {
+        if(other.CompareTag("Terrain"))
+        {
+            Destroy(gameObject);
+        }
+        else if(other.CompareTag("Player"))
+        {
+            //Remove Hp/kill cartman command, then delete this rock.
+        }
+    }
+}
