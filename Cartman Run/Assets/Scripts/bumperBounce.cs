150b35e3fc474551a3f64e61280704b9e7a24c82
JimmyVng <jvongpho@asu.edu>
2024-09-15 11:08:43 -0700

    Added Assets + Increased physics
    - Added slide
    - Added terrain rocks
    - Increased physics time to 2.5
    - TODO: Bumper function

--- Cartman Run/Assets/Scripts/bumperBounce.cs
+++ Cartman Run/Assets/Scripts/bumperBounce.cs
@@ -2,34 +2,41 @@
  * CPI 211 Game Jam 1
  * - Jimmy Vongphosy 
  * 
- * Description: Script that created the 'bumper' functionalities for obstacles
+ * Description: Script that created the 'bumper' functionalities for obstacles while ignoring the floor's collision
  * 
  */
-
 
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BounceOnCollision : MonoBehaviour
+public class PlayerBounce : MonoBehaviour
 {
-    public float bounceForce = 10f; 
+    public float bounceForce = 10f;
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.CompareTag("Player")) //If player tag
+        Transform collidedTransform = collision.gameObject.transform;
+
+        while (collidedTransform != null)
         {
-            Debug.Log("Player Touched Bumper"); //Known issue: Collision now detected
-
-            Rigidbody rb = collision.gameObject.GetComponent<Rigidbody>();
-
-            if (rb != null)
+            if (collidedTransform.CompareTag("Bumper")) //Issue: Collison keeps detecting parent object and not children tagged "Bumper"
             {
-                Vector3 bounceDir = collision.contacts[0].normal;
-
-                rb.AddForce(bounceDir * bounceForce, ForceMode.Impulse);
+                Debug.Log("Collision detected with bumper: " + collidedTransform.name);
+                Rigidbody rb = GetComponent<Rigidbody>();
+                if (rb != null)
+                {
+                    Vector3 bounceDirection = collision.contacts[0].normal;
+                    rb.AddForce(bounceDirection * bounceForce, ForceMode.Impulse);
+                }
+                return;
             }
+            collidedTransform = collidedTransform.parent;
         }
+
+        Debug.Log("Collision detected with: " + collision.gameObject.name);
     }
 }
+
+
 
