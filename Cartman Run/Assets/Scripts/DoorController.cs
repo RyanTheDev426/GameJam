16ff759e507c6a150456fce66416c8aac1f23056
Hyozzun <martin.mozier@gmail.com>
2024-09-16 09:01:04 -0700

    Final Final Exit.
    Added win text and 'Esc' to get back to main menu

--- Cartman Run/Assets/Scripts/DoorController.cs
+++ Cartman Run/Assets/Scripts/DoorController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class DoorController : MonoBehaviour
@@ -8,6 +9,7 @@ public class DoorController : MonoBehaviour
     [SerializeField] private bool openTrigger = false;
     [SerializeField] private bool closeTrigger = false;
     [SerializeField] private int numHats;
+    public TextMeshProUGUI winText;
     private int hat_count = 0;
     public collectibleScript hat;
 
@@ -21,11 +23,12 @@ public class DoorController : MonoBehaviour
             if (openTrigger && hat_count >= numHats)
             {
                 myDoor.Play("Door Opening", 0, 0.0f);
-                gameObject.SetActive(false);
+                gameObject.SetActive(false);  
             }
             else if (closeTrigger)
             {
                 myDoor.Play("Door Closing", 0, 0.0f);
+                winText.text = "You win! Press 'Esc' to go back to main menu.";
                 gameObject.SetActive(false);
             }
         }
