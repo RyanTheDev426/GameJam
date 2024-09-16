60bb8e8261c79000f3341ac07d4535d31b248485
RyanTheDev426 <orth.ryane@gmail.com>
2024-09-15 19:33:03 -0700

    rock behavior advanced

--- Cartman Run/Assets/Scripts/RockBehavior.cs
+++ Cartman Run/Assets/Scripts/RockBehavior.cs
@@ -1,20 +1,22 @@
 using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class RockBehavior : MonoBehaviour
 {
+    float moveSpeed = 5f;
     // Start is called before the first frame update
     void Start()
     {
-        Vector3 rockVelocity = new Vector3(10, 0, 0);
-
         GameObject playerObject = GameObject.FindGameObjectWithTag("Player");
         transform.LookAt(playerObject.transform.position);
-
     }
-
+    void Update()
+    {
+        transform.position += transform.forward * moveSpeed * Time.deltaTime;
+    }
     void OnColliderEnter(Collider other)
     {
         if(other.CompareTag("Terrain"))
